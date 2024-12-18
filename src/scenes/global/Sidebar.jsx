a1261e19baf542e206aeac345e4846a43d5b2843
rani93 <rni931849@gmail.com>
2024-12-18 13:17:05 +0530

    name

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -107,10 +107,10 @@ const Sidebar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  Ed Roh
+                  Rani Singh
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
-                  VP Fancy Admin
+                  College Student
                 </Typography>
               </Box>
             </Box>
